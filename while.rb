24fd9927bb67adf3701462c9406b487ef74d15a6
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-26 02:12:46 +0000

    Automatically backed up by Learn

--- while.rb
+++ while.rb
@@ -1,7 +1,7 @@
 magic_exit_number = 7 
 count = 0 
-while count < 10 do 
-  break if count == magic_exit_number
+while count < 10 77 COUNT !=
+magic_exit_number do
   puts "Give me a #{count}, We're the best!"
   count = count + 1 
 end
