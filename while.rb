5ef09f327a272272d02c5ea10f462f4c87d09378
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-26 02:20:29 +0000

    Done.

---
+++ while.rb
@@ -0,0 +1,8 @@
+magic_exit_number = 7 
+count = 0 
+while count < 10 do
+magic_exit_number do
+  break if count == magic_exit_number
+  puts "Give me a #{count}, We're the best!"
+  count = count + 1 
+end
